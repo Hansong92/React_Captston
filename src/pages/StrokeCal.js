26707b5a261cb7d386f23f7b26d64ea76ab1bffc
tw1lly <44590728+tw1lly@users.noreply.github.com>
2023-11-02 17:41:33 -0400

    Update StrokeCal.js
    Changed form into table format

--- src/pages/StrokeCal.js
+++ src/pages/StrokeCal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, TextField, Button, Typography, Paper, Select, MenuItem } from '@mui/material';
+import { Box, TextField, Button, Typography, Paper, Select, MenuItem, Table, TableBody, TableCell, TableRow } from '@mui/material';
 import { Container } from '@mui/system';
 
 const StrokeCal = () => {
@@ -40,126 +40,140 @@ const StrokeCal = () => {
       }}
     >
       <form onSubmit={handleSubmit}>
-        <div>
-          <Typography variant="h6">Gender</Typography>
-          <Select
-            name="gender"
-            value={formData.gender}
-            onChange={handleChange}
-          >
-            <MenuItem value="">Select Gender</MenuItem>
-            <MenuItem value="Male">Male</MenuItem>
-            <MenuItem value="Female">Female</MenuItem>
-          </Select>
-        </div>
-        <div>
-          <Typography variant="h6">Age</Typography>
-          <TextField
-            name="age"
-            type="number"
-            placeholder="Enter Age"
-            value={formData.age}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <Typography variant="h6">Hypertension/High Blood Pressure</Typography>
-          <Select
-            name="hypertension"
-            value={formData.hypertension}
-            onChange={handleChange}
-          >
-            <MenuItem value="">Select Hypertension/High Blood Pressure</MenuItem>
-            <MenuItem value="Yes">Yes</MenuItem>
-            <MenuItem value="No">No</MenuItem>
-          </Select>
-        </div>
-        <div>
-          <Typography variant="h6">Heart Disease</Typography>
-          <Select
-            name="heartDisease"
-            value={formData.heartDisease}
-            onChange={handleChange}
-          >
-            <MenuItem value="">Select Heart Disease</MenuItem>
-            <MenuItem value="Yes">Yes</MenuItem>
-            <MenuItem value="No">No</MenuItem>
-          </Select>
-        </div>
-        <div>
-          <Typography variant="h6">Marital Status</Typography>
-          <Select
-            name="maritalStatus"
-            value={formData.maritalStatus}
-            onChange={handleChange}
-          >
-            <MenuItem value="">Select Marital Status</MenuItem>
-            <MenuItem value="Married">Married</MenuItem>
-            <MenuItem value="Single">Single</MenuItem>
-          </Select>
-        </div>
-        <div>
-          <Typography variant="h6">Work Type</Typography>
-          <Select
-            name="workType"
-            value={formData.workType}
-            onChange={handleChange}
-          >
-            <MenuItem value="">Select Work Type</MenuItem>
-            <MenuItem value="Private">Private</MenuItem>
-            <MenuItem value="Self-employed">Self-employed</MenuItem>
-            <MenuItem value="Govt_job">Govt Job</MenuItem>
-            <MenuItem value="Stay_at_home">Stay at Home</MenuItem>
-            <MenuItem value="Never_worked">Never Worked</MenuItem>
-          </Select>
-        </div>
-        <div>
-          <Typography variant="h6">Residence</Typography>
-          <Select
-            name="residence"
-            value={formData.residence}
-            onChange={handleChange}
-          >
-            <MenuItem value="">Select Residence</MenuItem>
-            <MenuItem value="Urban">Urban</MenuItem>
-            <MenuItem value="Rural">Rural</MenuItem>
-          </Select>
-        </div>
-        <div>
-          <Typography variant="h6">Glucose Level</Typography>
-          <TextField
-            name="glucoseLevel"
-            type="number"
-            placeholder="Enter Glucose Level"
-            step="0.01"
-            value={formData.glucoseLevel}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <Typography variant="h6">BMI</Typography>
-          <TextField
-            name="bmi"
-            type="number"
-            placeholder="Enter BMI"
-            step="0.1"
-            value={formData.bmi}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <Typography variant="h6">Smoking Status</Typography>
-          <Select
-            name="smokingStatus"
-            value={formData.smokingStatus}
-            onChange={handleChange}
-          >
-            <MenuItem value="">Select Smoking Status</MenuItem>
-            <MenuItem value="Never_smoked">Never Smoked</MenuItem>
-            <MenuItem value="formerly_smoked">Formerly Smoked</MenuItem>
-            <MenuItem value="smokes">Smokes</MenuItem>
-          </Select>
-        </div>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <Typography variant="h6">Gender</Typography>
+                <Select
+                  name="gender"
+                  value={formData.gender}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">Select Gender</MenuItem>
+                  <MenuItem value="Male">Male</MenuItem>
+                  <MenuItem value="Female">Female</MenuItem>
+                </Select>
+              </TableCell>
+              <TableCell>
+                <Typography variant="h6">Age</Typography>
+                <TextField
+                  name="age"
+                  type="number"
+                  placeholder="Enter Age"
+                  value={formData.age}
+                  onChange={handleChange}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography variant="h6">Hypertension/High Blood Pressure</Typography>
+                <Select
+                  name="hypertension"
+                  value={formData.hypertension}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">Select Hypertension/High Blood Pressure</MenuItem>
+                  <MenuItem value="Yes">Yes</MenuItem>
+                  <MenuItem value="No">No</MenuItem>
+                </Select>
+              </TableCell>
+              <TableCell>
+                <Typography variant="h6">Heart Disease</Typography>
+                <Select
+                  name="heartDisease"
+                  value={formData.heartDisease}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">Select Heart Disease</MenuItem>
+                  <MenuItem value="Yes">Yes</MenuItem>
+                  <MenuItem value="No">No</MenuItem>
+                </Select>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography variant="h6">Marital Status</Typography>
+                <Select
+                  name="maritalStatus"
+                  value={formData.maritalStatus}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">Select Marital Status</MenuItem>
+                  <MenuItem value="Married">Married</MenuItem>
+                  <MenuItem value="Single">Single</MenuItem>
+                </Select>
+              </TableCell>
+              <TableCell>
+                <Typography variant="h6">Work Type</Typography>
+                <Select
+                  name="workType"
+                  value={formData.workType}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">Select Work Type</MenuItem>
+                  <MenuItem value="Private">Private</MenuItem>
+                  <MenuItem value="Self-employed">Self-employed</MenuItem>
+                  <MenuItem value="Govt_job">Govt Job</MenuItem>
+                  <MenuItem value="Stay_at_home">Stay at Home</MenuItem>
+                  <MenuItem value="Never_worked">Never Worked</MenuItem>
+                </Select>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography variant="h6">Residence</Typography>
+                <Select
+                  name="residence"
+                  value={formData.residence}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">Select Residence</MenuItem>
+                  <MenuItem value="Urban">Urban</MenuItem>
+                  <MenuItem value="Rural">Rural</MenuItem>
+                </Select>
+              </TableCell>
+              <TableCell>
+                <Typography variant="h6">Glucose Level</Typography>
+                <TextField
+                  name="glucoseLevel"
+                  type="number"
+                  placeholder="Enter Glucose Level"
+                  step="0.01"
+                  value={formData.glucoseLevel}
+                  onChange={handleChange}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography variant="h6">BMI</Typography>
+                <TextField
+                  name="bmi"
+                  type="number"
+                  placeholder="Enter BMI"
+                  step="0.1"
+                  value={formData.bmi}
+                  onChange={handleChange}
+                />
+              </TableCell>
+              <TableCell>
+                <Typography variant="h6">Smoking Status</Typography>
+                <Select
+                  name="smokingStatus"
+                  value={formData.smokingStatus}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">Select Smoking Status</MenuItem>
+                  <MenuItem value="Never_smoked">Never Smoked</MenuItem>
+                  <MenuItem value="formerly_smoked">Formerly Smoked</MenuItem>
+                  <MenuItem value="smokes">Smokes</MenuItem>
+                </Select>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
         <Button type="submit" variant="contained" color="primary">
           Submit
         </Button>
