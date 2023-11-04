9472074367fac44240d306726f2c1c0bb0015e3d
tw1lly <44590728+tw1lly@users.noreply.github.com>
2023-11-03 22:44:30 -0400

    Update App.js
    Now includes Model Explanation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import Main from './pages/Main.js'
 import React from 'react';
-
-
-import StrokeCal from './pages/StrokeCal.js'
-import MRI from './pages/MRI.js'
-import Navbar from "./components/Navbar.js";
-
-import  { BrowserRouter as Router ,Routes, Route} from 'react-router-dom';   
-import { Container } from '@mui/material';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Main from './pages/Main';
+import StrokeCal from './pages/StrokeCal';
+import MRI from './pages/MRI';
+import ModelExplanation from './pages/ModelExplanation'; 
 
 function App() {
   return (
     <Router>
-      <Navbar/>
-        <Routes> 
-          <Route path='/' element={<Main />} />
-          <Route path='MRI' element={<MRI />} />
-          <Route path='StrokeCal' element={<StrokeCal />} />
-        </Routes>
-    
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/MRI" element={<MRI />} />
+        <Route path="/StrokeCal" element={<StrokeCal />} />
+        <Route path="/model-explanation" element={<ModelExplanation />} />
+      </Routes>
     </Router>
-    
   );
 }
 
