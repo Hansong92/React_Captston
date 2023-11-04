3df805045e54bafe3d6ffbf319906b90b7ef2018
tw1lly <44590728+tw1lly@users.noreply.github.com>
2023-11-03 22:40:56 -0400

    Update MRI.js
    Button change into more compact button

--- src/pages/MRI.js
+++ src/pages/MRI.js
@@ -1,64 +1,54 @@
 import React, { useState } from 'react';
-import { Box, Button, Typography,Input } from '@mui/material';
+import { Box, Button, Typography, Input } from '@mui/material';
 import UploadIcon from '@mui/icons-material/Upload';
 import { visuallyHidden } from '@mui/utils';
 
-
-
 const MRI = () => {
-
-  const [selectedFile, setSelectedFile] = useState(null)
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file) {
       setSelectedFile(file.name);
-    }
-    else {
+    } else {
       setSelectedFile(null);
     }
   };
 
   return (
-    
-    
-     
-  
     <Box
-        margin='auto'
-        display="grid"   
-        justifyContent="center"
-        alignItems="center"
-        minHeight="50vh"
+      margin='auto'
+      display="grid"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="70vh" 
     >
+      <Typography
+        variant='h4'
+        fontFamily='Raleway'
+        fontWeight='bold'
+        color='#404040'
+      >
+        Check for Brain Tumors with Your Images
+      </Typography>
 
-    <Typography 
-    variant='h4'
-    fontFamily='Raleway' 
-    fontWeight='bold'
-    color='#404040'
-
-
-    >Upload images and find out if you have a possible brain tumor today</Typography>
-
-    <Button 
-        size='small'
-        component="label" 
-        variant="contained" 
-        
-
+      <Button
+        size='large' 
+        component="label"
+        variant="contained"
         startIcon={<UploadIcon />}
-    >  Upload 
-        <Input type="file" style={visuallyHidden} accept="image/jpeg, image/png, image/jpg" onChange={handleFileChange}/>
-      
-    </Button>    
-    {selectedFile && (
-        <Typography variant='body1' color='textSecondary'>
-          Selected File: {selectedFile} {/* Display the selected file name if available */}
+      >
+        Upload MRI Image
+        <Input type="file" style={visuallyHidden} accept="image/jpeg, image/png, image/jpg" onChange={handleFileChange} />
+      </Button>
+
+      {selectedFile && (
+        <Typography variant='body1' color='textSecondary' style={{ marginTop: '20px' }}>
+          Selected File: {selectedFile}
         </Typography>
-      )}  
-      </Box>  
-    );
+      )}
+    </Box>
+  );
 }
 
 export default MRI;
